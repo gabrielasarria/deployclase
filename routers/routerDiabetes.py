0e5ead8b64cd85174759b54e9405f84911802777
gabrielasarria <55821371+gabrielasarria@users.noreply.github.com>
2024-09-28 14:19:33 -0500

    Add files via upload

---
+++ routers/routerDiabetes.py
@@ -0,0 +1,48 @@
+import pickle
+from fastapi import APIRouter
+from Diabetesdata import Diabetesdata
+from schemas import schemas
+import numpy as np
+
+#el primero es nombre del archivo, asignatura.py y estamos importando asignatura que es la clase
+
+app = APIRouter()
+
+pkl_filename = ("RFDiabetesv102.pkl")
+with open(pkl_filename,'rb') as file:
+    model = pickle.load(file)
+
+labels = ["Sano","Posible diabetes"]
+
+@app.get("/")
+async def root():
+    return{
+        "message":"AI service"
+    }
+
+@app.post("/predict")
+def predict_diabetes(data:schemas.DiabetesData):
+    data = data.model_dump()
+    Pregnancies = data['Pregnancies']
+    Glucose = data['Glucose']
+    BloodPressure = data['BloodPressure']
+    SkinThickness = data['SkinThickness']
+    Insulin = data['Insulin']
+    BMI = data['BMI']
+    DiabetesPedigreeFunction = data['DiabetesPedigreeFunction']
+    Age = data['Age']
+
+    xin = np.array([Pregnancies,
+                    Glucose,
+                    BloodPressure,
+                    SkinThickness,
+                    Insulin , 
+                    BMI,
+                    DiabetesPedigreeFunction,
+                     Age]).reshape(1,8)
+    prediction = model.prediction(xin)
+    yout = labels[prediction[0]]
+
+    return {
+        'prediction': yout
+    }
